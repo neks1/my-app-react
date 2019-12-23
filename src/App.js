1b975dd102cc708c900a66bb27eb8803f0895421
Mykola Senyk <mykola.senyk12@gmail.com>
2019-12-23 17:54:26 +0200

    Created App for my project.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>CSS</li>
+        <li>HTML</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
