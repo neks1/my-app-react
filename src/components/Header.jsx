d2ccee1c74794a4b69cb9f1eda268ee0721fbcf9
Mykola Senyk <mykola.senyk12@gmail.com>
2019-12-26 16:43:59 +0200

    Added components. Created page.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://i.pinimg.com/originals/56/a6/73/56a6732a1210df59f321e89a9920b191.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
