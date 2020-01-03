d9d4a4bc0a9e0a68094ff719ab8dcde8e4fd1214
Mykola Senyk <mykola.senyk12@gmail.com>
2020-01-03 18:10:36 +0200

    Moved content to indexJs and used props.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,26 +4,8 @@ import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
 const Dialogs = (props) => {
-
-    let dialogs = [
-        {id: 1, name: 'Kolia'},
-        {id: 2, name: 'Fedya'},
-        {id: 3, name: 'Illia'},
-        {id: 4, name: 'Misha'},
-        {id: 5, name: 'Olya'},
-        {id: 6, name: 'Sasha'}
-    ]
-
-    let messages = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Yo'},
-        {id: 4, message: 'Yo'},
-        {id: 5, message: 'Yo'}
-    ]
-
-    let dialogsElements = dialogs.map( dialog => <DialogItem name={dialog.name} id={dialog.id} />);
-    let messsagesElements = messages.map( message => <Message message={message.message} />);
+    let dialogsElements = props.dialogs.map( dialog => <DialogItem name={dialog.name} id={dialog.id} />);
+    let messsagesElements = props.messages.map( message => <Message message={message.message} />);
 
     return (
         <div className={classes.dialogs}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = () => {
-    let posts = [
-        {id: 1, message: 'Hi, how are  you?', likesCount: 12},
-        {id: 2, message: 'It\'s my first post', likesCount: 11},
-        {id: 3, message: 'Bla bla', likesCount: 123},
-        {id: 4, message: 'Yo yo yo', likesCount: 123}
-    ]
-
-    let postsElements = posts.map(posts => <Post message={posts.message} likesCount={posts.likesCount} /> );
+const MyPosts = (props) => {
+    let postsElements = props.posts.map(posts => <Post message={posts.message} likesCount={posts.likesCount} /> );
 
     return (
         <div className={classes.myPosts}>
